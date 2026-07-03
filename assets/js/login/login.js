20999e29f7159dd4342541f4d1830035cfb12142
AGUSTÍN EZEQUIEL SMITH OCAMPO <57523083@ceibal.edu.uy>
2026-07-02 21:13:04 -0300

    Fix: Erroeres generales en el login y landing page

---
+++ assets/js/login/login.js
@@ -0,0 +1,11 @@
+document.QuerySelector('.section-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    if (!email || !password) {
+        alert('Por favor, complete todos los campos');
+        return;
+    }
+})
