0ed61e9dcd1fc2da3500a2370de6e6a36823055c
AGUSTÍN EZEQUIEL SMITH OCAMPO <57523083@ceibal.edu.uy>
2026-08-19 18:34:06 -0300

    feat(dashboard.html): Se agrego el menu (slide) en dispositivo mobile

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,7 +1,28 @@
 // Menu
 const menuBtn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
+const menuBtnClose = document.getElementById("menu-btn-close");
 
+// Boton de menu
 menuBtn.addEventListener("click", () => {
     menu.classList.toggle("active");
+});
+
+// Boton de cerrar menu
+menuBtnClose.addEventListener("click", () => {
+    menu.classList.remove("active");
+});
+
+// Al hacer scroll, ocultar el menu (En mobile)
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100 && window.innerWidth < 768) {
+        menu.classList.remove("active");
+    }
+});
+
+// Al presionar notificaciones, ocultar el menu (En mobile)
+document.getElementById("notifBtn").addEventListener("click", () => {
+    if (window.innerWidth < 768) {
+        menu.classList.remove("active");
+    }
 });
--- assets/js/notif.js
+++ assets/js/notif.js
@@ -2,6 +2,19 @@
 const notifBtn = document.getElementById("notifBtn");
 const notifPanel = document.getElementById("notifPanel");
 
+// Boton de notificaciones
 notifBtn.addEventListener("click", () => {
     notifPanel.classList.toggle("active");
+});
+
+// Ocultar notificaciones al hacer scroll
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+        notifPanel.classList.remove("active");
+    }
+});
+
+// Si se presiona el menu, ocultar notificaciones
+document.getElementById("menu-btn").addEventListener("click", () => {
+    notifPanel.classList.remove("active");
 });
