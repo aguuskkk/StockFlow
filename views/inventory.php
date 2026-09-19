5133e5b27731edaf99c96d6d6d462c720beb556b
AGUSTÍN EZEQUIEL SMITH OCAMPO <57523083@ceibal.edu.uy>
2026-09-18 21:31:37 -0300

    refactor: Se arreglaron pequeños errores en el codigo (correigir la estructuras de tablas, algunas etiquetas mal cerradas, etc)

--- views/inventory.php
+++ views/inventory.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -135,6 +135,7 @@
                                 <div class="notif-content">
                                     <p class="notif-text"><strong>Teclado Gaming</strong> está por debajo del stock mínimo.</p>
                                     <span class="notif-time">Hace 12 minutos</span>
+                                </div>
                             </li>
                             <li class="notif-item unread">
                                 <i class="fa-solid fa-box"></i>
